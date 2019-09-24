4cb74f1d80a68496522a8cf58f9fa24f18dba33d
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-09-24 13:55:42 -0700

    Added connection for controls

--- react-tetris/src/components/Controls.js
+++ react-tetris/src/components/Controls.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 import './css/Controls.css'
 
 class Controls extends Component {
   render() {
+    const { isRunning } = this.props
     return (
       <div className="controls">
         {/* left */}
         <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.moveLeft()
         }}>Left</button>
         {/* right */}
         <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.moveRight()
         }}>Right</button>
         {/* rotate */}
         <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.rotate()
         }}>Rotate</button>
         {/* down */}
         <button className="control-button" onClick={(e) => {
+          if (!isRunning) { return }
+          this.props.moveDown()
         }}>Down</button>
       </div>
     )
   }
 }
-export default Controls
+
+const mapDispatchToProps = () => {
+  return {
+      moveRight,
+      moveLeft,
+      moveDown,
+      rotate
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+      isRunning: state.game.isRunning
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps())(Controls)

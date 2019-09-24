2a88d6429bb44d73cb54a6d98c95c9e7e94b4e08
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-09-24 14:29:46 -0700

    move left and right

--- react-tetris/src/reducers/GameReducer.js
+++ react-tetris/src/reducers/GameReducer.js
@@ -17,8 +17,14 @@ import {
       }
       return state
       case MOVE_RIGHT:
+        if(canMoveTo(shape, grid, x - 1, y, rotation)){
+          return { ...state, x: x - 1 }
+        }
         return state
       case MOVE_LEFT:
+          if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+            return { ...state, x: x + 1 }
+          }
         return state
       case MOVE_DOWN:
         return state
